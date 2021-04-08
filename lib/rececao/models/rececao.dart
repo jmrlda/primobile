30233effd6e10be164447bfdb99ff818a4e9085d
guirruta <dercioguirruta44@gmail.com>
2021-04-08 09:08:44 +0200

    Transformar  objecto receção em json

--- lib/rececao/models/rececao.dart
+++ lib/rececao/models/rececao.dart
@@ -1,11 +1,14 @@
 import 'dart:convert';
 
+import 'package:primobile/rececao/models/models.dart';
+
 class Rececao {
   int rececao;
   String data;
-  // List<ArtigoExpedicao> listaArtigo;
-  String listaArtigo;
+  List<ArtigoRececao> listaArtigo;
+  // String listaArtigo;
   String usuario;
+  List artigosJson = List();
 
   Rececao({this.rececao, this.data, this.listaArtigo, this.usuario});
 
@@ -27,16 +30,34 @@ class Rececao {
     // String numContrib = json['numContrib'] == "" ? "0" : data['numContrib'];
     // numContrib = numContrib.replaceAll(" ", "");
     return Rececao(
-      rececao: json['rececao'],
-      data: json['data'],
-      listaArtigo: json['listaArtigo'],
-      usuario: json['usuario'],
+      rececao: json['Número Documento'],
+      usuario: json['Utilizador'],
     );
+
     // data['imagemBuffer'] == null ? null : data['imagemBuffer']);
   }
 
   List<Rececao> parseRececao(String response) {
     final parsed = json.decode(response).cast<Map<String, dynamic>>();
     return parsed.map<Rececao>((json) => Rececao.fromJson(json)).toList();
+  }
+
+  List toMapApi() {
+    int i = 0;
+    artigosJson.clear();
+    if (listaArtigo != null) {
+      listaArtigo.forEach((element) {
+        artigosJson.add(element.toMap());
+      });
+    } else {
+      artigosJson = [];
+    }
+
+    return [
+      {
+        'rececao': this.rececao,
+        'artigos': artigosJson,
+      }
+    ];
   }
 }
