97db3058f8c387ea9beca46b91b5606bbb13ddfe
guirruta <dercioguirruta44@gmail.com>
2021-04-08 08:45:16 +0200

    Transformar  expedicao em json

--- lib/expedicao/models/expedicao.dart
+++ lib/expedicao/models/expedicao.dart
@@ -1,20 +1,21 @@
 import 'dart:convert';
 
 import 'package:primobile/expedicao/models/artigo_expedicao.dart';
+import 'package:primobile/sessao/sessao_api_provider.dart';
 
 class Expedicao {
   int expedicao;
   String data;
-  // List<ArtigoExpedicao> listaArtigo;
-  String listaArtigo;
+  List<ArtigoExpedicao> listaArtigo;
+  // String listaArtigo;
   String usuario;
+  List artigosJson = List();
 
   Expedicao({this.expedicao, this.data, this.listaArtigo, this.usuario});
 
   factory Expedicao.fromMap(Map<String, dynamic> json) => new Expedicao(
         expedicao: json['expedicao'],
         data: json['data'],
-        listaArtigo: json['listaArtigo'],
         usuario: json['usuario'],
       );
 
@@ -40,5 +41,32 @@ class Expedicao {
   List<Expedicao> parseExpedicao(String response) {
     final parsed = json.decode(response).cast<Map<String, dynamic>>();
     return parsed.map<Expedicao>((json) => Expedicao.fromJson(json)).toList();
+  }
+
+  void setListaArtigo(List<ArtigoExpedicao> artigos) {
+    this.listaArtigo = artigos;
+  }
+
+  void setArtigo(ArtigoExpedicao artigo) {
+    this.listaArtigo.add(artigo);
+  }
+
+  List toMapApi() {
+    int i = 0;
+    if (listaArtigo != null) {
+      listaArtigo.forEach((element) {
+        artigosJson.add(element.toMap());
+        //regrasPreco_json.add(regrasPreco[i++].toMap());
+      });
+    } else {
+      artigosJson = [];
+    }
+
+    return [
+      {
+        'expedicao': this.expedicao,
+        'artigos': artigosJson,
+      }
+    ];
   }
 }
