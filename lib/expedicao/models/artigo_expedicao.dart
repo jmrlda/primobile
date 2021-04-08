a92f2f51b101ca09951e5a29a6dadaa7ddeaa538
guirruta <dercioguirruta44@gmail.com>
2021-04-08 08:54:31 +0200

    Enviar dados de actualização de expedicao

--- lib/expedicao/models/artigo_expedicao.dart
+++ lib/expedicao/models/artigo_expedicao.dart
@@ -2,12 +2,16 @@ import 'dart:convert';
 
 import 'dart:ffi';
 
+import 'package:primobile/expedicao/models/models.dart';
+import 'package:primobile/sessao/sessao_api_provider.dart';
+import 'package:http/http.dart' as http;
+
 class ArtigoExpedicao {
   String artigo;
   String descricao;
   String unidade;
-  Double quantidadeExpedir;
-  Double quantidadePendente;
+  double quantidadeExpedir;
+  double quantidadePendente;
 
   ArtigoExpedicao(
       {this.artigo,
@@ -18,19 +22,16 @@ class ArtigoExpedicao {
 
   factory ArtigoExpedicao.fromMap(Map<String, dynamic> json) =>
       new ArtigoExpedicao(
-        artigo: json['artigo'],
-        descricao: json['descricao'],
-        unidade: json['unidade'],
-        quantidadeExpedir: json['quantidadeExpedir'],
-        quantidadePendente: json['quantidadePendente'],
-      );
+          artigo: json['artigo'],
+          descricao: json['descricao'],
+          quantidadeExpedir: json['quantidade_expedir'],
+          quantidadePendente: json['quantidade_pendente']);
 
   Map<String, dynamic> toMap() => {
         'artigo': artigo,
         'descricao': descricao,
-        'unidade': unidade,
-        'quantidadeExpedir': quantidadeExpedir,
-        'quantidadePendente': quantidadePendente,
+        'quantidade_expedir': quantidadeExpedir,
+        'quantidade_pendente': quantidadePendente,
       };
 
   factory ArtigoExpedicao.fromJson(Map<String, dynamic> json) {
@@ -39,9 +40,8 @@ class ArtigoExpedicao {
     return ArtigoExpedicao(
       artigo: json['artigo'],
       descricao: json['descricao'],
-      unidade: json['unidade'],
-      quantidadeExpedir: json['quantidadeExpedir'],
-      quantidadePendente: json['quantidadePendente'],
+      quantidadeExpedir: json['quantidade_expedir'],
+      quantidadePendente: json['quantidade_pendente'],
     );
     // data['imagemBuffer'] == null ? null : data['imagemBuffer']);
   }
@@ -51,5 +51,45 @@ class ArtigoExpedicao {
     return parsed
         .map<ArtigoExpedicao>((json) => ArtigoExpedicao.fromJson(json))
         .toList();
+  }
+
+  static Future<http.Response> postExpedicao(
+      Expedicao expedicao, List<ArtigoExpedicao> lista_artigo) async {
+    Map<String, dynamic> parsed = await SessaoApiProvider.read();
+    String protocolo = 'http://';
+    String host = SessaoApiProvider.base_url;
+    String rota = '/WebApi/ExpedicaoController/Expedicao/' +
+        expedicao.expedicao.toString();
+    var url = protocolo + host + rota;
+    http.Response response;
+    expedicao.listaArtigo = lista_artigo;
+    String body = json.encode(expedicao.toMapApi());
+
+    // json.encode(lista_artigo);
+
+    // var response = await http.post(url,
+    //     headers: {"Content-Type": "application/json"}, body: body);
+
+    try {
+      var sessao = await SessaoApiProvider.read();
+      if (sessao == null || sessao.length == 0) {
+        print('Ficheiro sessao nao existe');
+        return null;
+      } else {
+        String token = sessao['access_token'];
+        response = await http.post(url,
+            headers: {
+              "Authorization": "Bearer $token",
+              "Accept": "application/json"
+            },
+            body: body);
+      }
+    } catch (e) {
+      throw e;
+
+      // return 3;
+    }
+
+    return response;
   }
 }
