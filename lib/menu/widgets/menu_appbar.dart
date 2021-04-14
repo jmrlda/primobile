80a6503ddd66f7367edd50128a81a84c644d1808
guirruta <dercioguirruta44@gmail.com>
2021-04-14 10:34:09 +0200

    criar metodo  que identificada a opção selecionada e chama a sua respectiva ação ou rota

--- lib/menu/widgets/menu_appbar.dart
+++ lib/menu/widgets/menu_appbar.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:primobile/artigo/util.dart';
 import 'package:primobile/util/util.dart';
 
+BuildContext contexto;
+
 AppBar menuAppBar() {
   return new AppBar(
     title: new Center(
@@ -12,6 +14,7 @@ AppBar menuAppBar() {
       PopupMenuButton<String>(
         onSelected: opcaoAcao,
         itemBuilder: (BuildContext context) {
+          contexto = context;
           return Opcoes.escolha.map((String escolha) {
             return PopupMenuItem<String>(
               value: escolha,
@@ -22,4 +25,12 @@ AppBar menuAppBar() {
       )
     ],
   );
+}
+
+void opcaoAcao(String opcao) async {
+  if (opcao == Opcoes.Login) {
+    await Navigator.pushNamed(contexto, '/');
+  } else if (opcao == Opcoes.Configurar) {
+    await Navigator.pushNamed(contexto, '/config_instancia');
+  }
 }
